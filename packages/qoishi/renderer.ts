ea5fb91a6ccac14f90a64f0c6323fcc0d47f08c7
montmorill <montmorillonite@qq.com>
2025-04-06 21:43:16 +0800

    :fire: remove `console.log` stmts

--- packages/qoishi/renderer.ts
+++ packages/qoishi/renderer.ts
@@ -4,7 +4,5 @@ import {} from './preload'
 export const name = 'qoishi'
 
 export function apply(ctx: Context) {
-  console.log('apply qoishi/renderer', ctx)
-  console.log('window.baseDir', window.baseDir)
   ctx.set('baseDir', window.baseDir)
 }
